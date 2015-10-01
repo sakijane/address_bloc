5a1ebe1e263a693ec189f310030ad0aa457dcf1f
Saki Jane <sakicanbereached@gmail.com>
2015-10-01 15:12:10 -0700

    Assignment 21 revised

--- models/address_book.rb
+++ models/address_book.rb
@@ -19,10 +19,14 @@ class AddressBook
   end
 
   def remove_entry(name, phone_number, email)
+    delete_entry = nil
+
     @entries.each do |entry|
-      if name == entry.name
-        @entries.delete(index, Entry.new(name, phone_number, email))
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        delete_entry = entry
       end
     end
+
+    @entries.delete(delete_entry)
   end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -38,11 +38,19 @@ RSpec.describe AddressBook do
   end
 
   describe "#remove_entry" do
-    it "removes only one entry from the address book" do
+    it "removes an entry using the name, phone_number and email" do
       book = AddressBook.new
-      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Michael Faraday', '010.210.5181', 'michael.faraday@faraday.com')
 
-      expect(book.entries.size).to eq(0)
+      name = 'Ada Lovelace'
+      phone_number = '010.012.1815'
+      email = 'augusta.king@lovelace.com'
+      book.add_entry(name, phone_number, email)
+
+      expect(book.entries.size).to eq(2)
+      book.remove_entry(name, phone_number, email)
+      expect(book.entries.size).to eq(1)
+      expect(book.entries.first.name).to eq('Michael Faraday')
     end
   end
 end
