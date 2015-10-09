79e7fd6ca751a29e8404b6bbc8a5abb08a8d64f9
Saki Jane <sakicanbereached@gmail.com>
2015-10-09 16:54:46 -0700

    Checkpoint 24 completed, search

--- models/address_book.rb
+++ models/address_book.rb
@@ -38,6 +38,42 @@ class AddressBook
     end
   end
 
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+    while lower <= upper
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    return nil
+  end
+
+  def iterative_search(name)
+
+    @entries.detect { |entry| name == entry.name }
+#    @entries.each do |entry|
+#      if name == entry.name
+#        return entry
+#      else
+#        return nil
+#      end
+#    end
+#    if name == @entries(name)
+#      return entry
+#    else
+#      return nil
+#    end
+  end
+
   def remove_entry(name, phone_number, email)
     delete_entry = nil
 
